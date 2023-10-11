f06f4c520d56acd76c61a347484ba35e1503397c
lubbree <lubbree@gmail.com>
2023-10-11 18:05:34 -0400

    add some of the logic for the sim.

--- header.h
+++ header.h
@@ -1,8 +1,42 @@
-struct job
+#include <string>
+
+struct job_t
 {
-    int pid;
+    int pid;  //assumption -1 == no job
     int arrivalTime;
     int totalCpuTime;
     int ioFrequency;
     int ioDuration;
 };
+
+struct runningJob_t
+{
+    job_t job;
+    int time;
+};
+
+struct waitingJob_t
+{
+    job_t job;
+    int time;
+};
+
+enum State { New, Ready, Running, Waiting, Terminated };
+
+std::string printState(State s){
+    switch (s)
+    {
+    case New:
+        return "New";
+    case Ready:
+        return "Ready";
+    case Running:
+        return "Running";
+    case Waiting:
+        return "Waiting";
+    case Terminated:
+        return "Terminated";
+    default:
+       return "!State Not Found!";
+    }
+}
--- main.cpp
+++ main.cpp
@@ -1,17 +1,76 @@
 #include <stdio.h>
 #include <deque>
+#include <vector>
 #include <iostream>
+#include <string>
+#include <format>
 #include "header.h"
 
+using namespace std;
+
+void printJob(int time, job_t job, State old, State current){
+    //return "time: " + time + " pid " + job.pid + " Old State: " + printState(old) + " New State " + printState(current) + " \n";
+    //return format("Time: {} | pid: {} | Old State: {} | New State: {} \n", time, job.pid, printState(old), printState(current));
+    printf("Time: %d | pid: %d | Old State: %s | New State: %s \n", time, job.pid, printState(old), printState(current));
+    //cout << "Time: " << time << " pid: " << job.pid << " Old State: " << printState(old) << " New State " << printState(current) << "\n";
+}
 
 int main()
 {
     int clock = 0;
+
+    vector<job_t> loaded; //new is a protected term hopefully you can just add stuff into the vector
+    deque<job_t> ready;
+    runningJob_t running;
+    vector<waitingJob_t> waiting;
     
-    struct job j1;
+    struct job_t j1;
+    j1.pid = 0;
     j1.arrivalTime = 1;
 
-    printf("hello world");
+    loaded.push_back(j1);
+
+    while(!loaded.empty() && !ready.empty() && running.job.pid != -1 && !waiting.empty() || clock < 5){ //exit when done
+
+        //from new into ready
+        if (!loaded.empty()){
+            vector<job_t>::iterator iter = loaded.begin();
+
+            for(iter; iter < loaded.end(); iter++){
+                if(iter->arrivalTime <= clock){
+                    ready.push_back(*iter);
+                    loaded.erase(iter);
+                    printJob(clock, *iter, New, Ready);
+                }
+            }
+        }
+
+        //move from waiting to ready
+        if (!waiting.empty()){
+            vector<waitingJob_t>::iterator iter = waiting.begin();
+
+            for(iter; iter < waiting.end(); iter++){
+                if(iter->job.ioDuration <= iter->time){
+                    ready.push_back(iter->job);
+                    waiting.erase(iter);
+                    printJob(clock, iter->job, New, Ready);
+                }
+                iter->time += 1;
+            }
+        }
+        /*
+        if (running.job.pid > 0) {
+            if (running.job.ioFrequency <= running.time){
+                waiting.push_back(running.job);
+                break;
+            }
+        }
+        */
+
+        clock++;
+    }
+
+    cout << loaded.empty();
 
     return 0;
 }
